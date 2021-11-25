245858fe5191b1f8bc814910c037a8de8d054687
Marvin Simon <simon.marvin.1@web.de>
2021-11-25 03:37:50 +0100

    Überarbeitung der Positionen

--- aufgabe2/freelist.c
+++ aufgabe2/freelist.c
@@ -5,77 +5,95 @@
 
 #define MAIN_MEMORY 1000
 
+void initTheArray();
 static int restMemory = MAIN_MEMORY;
 
 typedef struct FreeList {
     int reservedMemory;
+    int location;
     bool reserved;
 }FreeList_t;
 
-static FreeList_t freeListMemory [MAIN_MEMORY] = {0, false};
+bool initArray = false;
+static FreeList_t freeListMemory [MAIN_MEMORY] = {0, -1, false};
 
 int position(int size) {
-    for(int i = 0; i < MAIN_MEMORY; i++) {
-        if(restMemory < size) {
-            return -1;
-        }
-        if(!freeListMemory[i].reserved
-        && (freeListMemory[i].reservedMemory >= size || freeListMemory[i].reservedMemory == 0)) {
-            return i;
+    initTheArray();
+    int openSpace = 0;
+    if(restMemory < size) {
+        return -1;
+    }
+    for (int i = 0; i < MAIN_MEMORY; i++) {
+        if(!freeListMemory[i].reserved) {
+            openSpace++;
+            if(openSpace == size) {
+                return i - openSpace + 1;
+            }
+        } else {
+            openSpace = 0;
         }
     }
     return -1;
 }
 
 void insert(int position, int size) {
-
-    if(!freeListMemory[position].reserved
-    && (freeListMemory[position].reservedMemory == size || freeListMemory[position].reservedMemory == 0)
-    && restMemory >= size)
-    {
-        freeListMemory[position].reserved = true;
-        freeListMemory[position].reservedMemory = size;
-        restMemory -= size;
-    } else {
-        for (int i = 0; i < MAIN_MEMORY; i++) {
-            if (!freeListMemory[i].reserved
-                && (freeListMemory[i].reservedMemory >= size)
-                && restMemory >= size) {
-                freeListMemory[i].reserved = true;
-                if (freeListMemory[i].reservedMemory != 0 && freeListMemory[i].reservedMemory != size) {
-                    FreeList_t buffer = freeListMemory[i + 1];
-                    freeListMemory[i + 1].reservedMemory = freeListMemory[i].reservedMemory - size;
-                    freeListMemory[i].reservedMemory = size;
-                    restMemory -= size;
-                    freeListMemory[i + 1].reserved = false;
-                    for (int j = i + 2; j < MAIN_MEMORY; j++) {
-                        FreeList_t buffer2 = freeListMemory[j];
-                        freeListMemory[j] = buffer;
-                        buffer = buffer2;
-                    }
-                }
+    initTheArray();
+    if(restMemory < size) {
+        return;
+    }
+    int openSpace = 0;
+    for (int i = position; i < position+size; i++) {
+        if(!freeListMemory[i].reserved) {
+            openSpace++;
+            if(openSpace == size) {
                 break;
             }
+        } else {
+            openSpace = 0;
         }
     }
+    if(openSpace == size) {
+        freeListMemory[position].location = position;
+        freeListMemory[position].reservedMemory = size;
+        restMemory -= size;
+        for(int i = position; i < position+size; i++) {
+            freeListMemory[i].reserved = true;
+        }
+    }
+
     printf("\n");
     for (int i = 0; i < MAIN_MEMORY; ++i) {
-        if(freeListMemory[i].reserved) {
+        if(freeListMemory[i].reservedMemory != 0) {
             printf("Position %d, Size %d -> ", i, freeListMemory[i].reservedMemory);
         }
     }
 }
 void free(int position) {
-    if(position < MAIN_MEMORY) {
-        freeListMemory[position].reserved = false;
-        restMemory += freeListMemory[position].reservedMemory;
+    initTheArray();
+    if(freeListMemory[position].location == position) {
+        for (int i = position; i < position+freeListMemory[position].reservedMemory; ++i) {
+            freeListMemory[i].reserved = false;
+        }
+        freeListMemory[position].location = 0;
+        freeListMemory[position].reservedMemory = 0;
     }
 
     printf("\n");
 
     for (int i = 0; i < MAIN_MEMORY; ++i) {
-        if(freeListMemory[i].reserved) {
+        if(freeListMemory[i].reservedMemory != 0) {
             printf("Position %d, Size %d -> ", i, freeListMemory[i].reservedMemory);
         }
     }
+}
+
+void initTheArray() {
+    if(!initArray) {
+        for (int i = 0; i < MAIN_MEMORY; ++i) {
+            freeListMemory[i].reservedMemory = 0;
+            freeListMemory[i].location = -1;
+            freeListMemory[i].reserved = false;
+        }
+    }
+    initArray = true;
 }
